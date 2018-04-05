c499528600ab16590c3b7d966f50a7ba24225518
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-04-05 12:38:25 -0500

    focomovimiento

--- screen.cs
+++ screen.cs
@@ -11,14 +11,15 @@ namespace ConsoleApplication1
 {
     class screen:GameWindow
     {
+
         Punto[] punto = new Punto[10000];
         Punto foco= new Punto();
         Random aleatorio= new Random();
         double intensidad;
         double radio;
-          public screen(int ancho, int alto): base (ancho,alto)
+
+        public screen(int ancho, int alto): base (ancho,alto)
         {
-            
 
         }
           protected override void OnLoad(EventArgs e)
@@ -27,7 +28,7 @@ namespace ConsoleApplication1
               radio = 0.2f;
               GL.LoadIdentity();
               GL.MatrixMode(MatrixMode.Projection);
-              GL.Ortho(0, 1, 0, 1, -1, 1);
+              GL.Ortho(0, 1, 0, 1, 0, 1);
               foco.valores(0.5,0.5,0);
               for (int i = 0; i < 10000; i++)
               {
@@ -48,8 +49,14 @@ namespace ConsoleApplication1
               base.OnRenderFrame(e);
               GL.Begin(PrimitiveType.Points);
               for(int i=0;i<10000;i++){
-                 intensidad=1/Math.Sqrt(Math.Pow(foco.x- punto[i].x ,2)+ Math.Pow(foco.y- punto[i].y ,2)+Math.Pow(foco.z- punto[i].z ,2));
-                 GL.Color3(intensidad * radio, intensidad * radio, intensidad * radio);
+
+                 intensidad=1/Math.Sqrt(
+                     Math.Pow(foco.x- punto[i].x ,2)+
+                     Math.Pow(foco.y- punto[i].y ,2)+
+                     Math.Pow(foco.z- punto[i].z ,2));
+                     
+
+                  GL.Color3(intensidad * radio, intensidad * radio, intensidad * radio);
                   GL.Vertex2(punto[i].x,punto[i].y);
               }
                GL.End();
@@ -57,6 +64,18 @@ namespace ConsoleApplication1
               this.SwapBuffers();
           }
 
+          protected override void OnKeyPress(KeyPressEventArgs e)
+          {
+              base.OnKeyPress(e);
+              if (e.KeyChar == 'a')
+              {
+                  foco.x += 0.1;
+              }
+              if (e.KeyChar == 's')
+              {
+                  foco.x -= 0.1;
+              }
+          }
 
 
 
