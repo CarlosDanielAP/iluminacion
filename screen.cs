99f159e7ddaf7f6f269f233aa9c04000395723b7
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-04-10 11:40:15 -0500

    Revert "mueve foco con mouse"
    This reverts commit 7faabfb35dd1b837335ad88dc2b33892d126e2c9.

--- screen.cs
+++ screen.cs
@@ -17,8 +17,6 @@ namespace ConsoleApplication1
         Random aleatorio= new Random();
         double intensidad;
         double radio;
-        double posx;
-        double posy;
 
         public screen(int ancho, int alto): base (ancho,alto)
         {
@@ -30,8 +28,8 @@ namespace ConsoleApplication1
               radio = 0.2f;
               GL.LoadIdentity();
               GL.MatrixMode(MatrixMode.Projection);
-              GL.Ortho(0, 1, 0, 1, 0, 1);
-              foco.valores(posx,posy,0);
+              GL.Ortho(-2, 2, -2, 2, -1, 10);
+              foco.valores(0.5,0.5,0);
               for (int i = 0; i < 10000; i++)
               {
                   punto[i] = new Punto();
@@ -63,7 +61,7 @@ namespace ConsoleApplication1
               GL.Vertex3(1, 1, 1);
               GL.End();*/
 
-             GL.Begin(PrimitiveType.Points);
+            /*  GL.Begin(PrimitiveType.Points);
               for(int i=0;i<10000;i++){
 
                  intensidad=1/Math.Sqrt(
@@ -75,8 +73,8 @@ namespace ConsoleApplication1
                   GL.Color3(intensidad * radio, intensidad * radio, intensidad * radio);
                   GL.Vertex2(punto[i].x,punto[i].y);
               }
-               GL.End();
-               foco.valores(posx, posy, 0);
+               GL.End();*/
+
               this.SwapBuffers();
           }
 
@@ -91,13 +89,6 @@ namespace ConsoleApplication1
               {
                   foco.x -= 0.1;
               }
-          }
-
-          protected override void OnMouseMove(OpenTK.Input.MouseMoveEventArgs e)
-          {
-              base.OnMouseMove(e);
-              posx = 0.001 * e.Mouse.X;
-              posy = 0.001 * e.Mouse.Y;
           }
 
 
