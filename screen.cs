2b03b77c642125d79be1605a136e32a58109465c
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-04-10 12:27:08 -0500

    ILUMINACION CUADRITO

--- screen.cs
+++ screen.cs
@@ -14,11 +14,13 @@ namespace ConsoleApplication1
 
         Punto[] punto = new Punto[10000];
         Punto foco= new Punto();
+        Punto []vertices=new Punto[4];
         Random aleatorio= new Random();
         double intensidad;
         double radio;
         double posx;
         double posy;
+        double distancia;
 
         public screen(int ancho, int alto): base (ancho,alto)
         {
@@ -30,15 +32,20 @@ namespace ConsoleApplication1
               radio = 0.2f;
               GL.LoadIdentity();
               GL.MatrixMode(MatrixMode.Projection);
-              GL.Ortho(0, 1, 0, 1, 0, 1);
-             
+              GL.Ortho(0, 1, 0, 1, -1, 1);
+              
+              foco.valores(0.5, 0.5, 0);
+              vertices[0] = new Punto(); vertices[0].valores(0f, 0f, 0f);
+              vertices[1] = new Punto(); vertices[1].valores(0.5f, 0f, 0f);
+              vertices[2] = new Punto(); vertices[2].valores(0.5f, 0.5f, 0f);
+              vertices[3] = new Punto(); vertices[3].valores(0f, 0.5f, 0.5f);
           }
 
           protected override void OnUpdateFrame(FrameEventArgs e)
           {
               base.OnUpdateFrame(e);
               GL.Clear(ClearBufferMask.ColorBufferBit);
-              GL.ClearColor(0f, 0f, 0f, 0f);
+              GL.ClearColor(1f, 0.5f, 0.5f, 0f);
           }
           protected override void OnRenderFrame(FrameEventArgs e)
           {
@@ -46,11 +53,23 @@ namespace ConsoleApplication1
 
         
 
-             GL.Begin(PrimitiveType.Points);
-            
-               GL.End();
-               foco.valores(posx, posy, 0);
-              this.SwapBuffers();
+             GL.Begin(PrimitiveType.Quads);
+
+
+
+
+             for (int i = 0; i < 4; i++)
+             {
+                 distancia=Math.Sqrt(
+                     Math.Pow(foco.x-vertices[i].x,2)+
+                     Math.Pow(foco.x-vertices[i].y,2)+
+                     Math.Pow(foco.x-vertices[i].z,2));
+                 GL.Color3(1 * distancia, 1 * distancia, 1 * distancia);
+                 GL.Vertex2(vertices[i].x,vertices[i].y);
+
+             }
+             GL.End();
+                 this.SwapBuffers();
           }
 
           protected override void OnKeyPress(KeyPressEventArgs e)
@@ -68,9 +87,9 @@ namespace ConsoleApplication1
 
           protected override void OnMouseMove(OpenTK.Input.MouseMoveEventArgs e)
           {
-             /* base.OnMouseMove(e);
+              base.OnMouseMove(e);
               posx = 0.001 * e.Mouse.X;
-              posy = 0.001 * e.Mouse.Y;*/
+              posy = 0.001 * e.Mouse.Y;
           }
 
 
