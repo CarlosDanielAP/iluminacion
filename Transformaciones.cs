cc22a25d3f70d93929e4ae3f2c83d35a6ffa7d20
papachanguito <carlos.ahuactzin@alumno.buap.mx>
2018-04-19 13:00:03 -0500

    algo

---
+++ Transformaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Transformaciones
+    {
+
+        public Transformaciones()
+        {
+
+        }
+
+        public Punto rotar(Punto vertice, double angulo)
+    {
+        vertice.x = vertice.x * Math.Cos(angulo) + vertice.y * Math.Sin(angulo);
+        vertice.y = -1 * vertice.x * Math.Sin(angulo) + vertice.y * Math.Cos(angulo);
+        vertice. z= vertice.z;
+        return vertice;
+        
+    }
+
+    }
+}
--- screen.cs
+++ screen.cs
@@ -13,6 +13,7 @@ namespace ConsoleApplication1
         Punto[] punto = new Punto[10000];
         Punto foco= new Punto();
         Punto []vertices=new Punto[8];
+        Transformaciones transformar=new Transformaciones();
         Random aleatorio= new Random();
         double intensidad;
         double radio;
@@ -31,11 +32,12 @@ namespace ConsoleApplication1
               radio = 0.2f;
               GL.LoadIdentity();
               GL.MatrixMode(MatrixMode.Projection);
-            Matrix4 matriz = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1, 50.0f);
-            GL.LoadMatrix(ref matriz);
-            GL.Enable(EnableCap.DepthTest);
+          //  Matrix4 matriz = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1, 50.0f);
+           // GL.LoadMatrix(ref matriz);
+              GL.Ortho(-5, 5, -5, 5, 0, 1);
+           // GL.Enable(EnableCap.DepthTest);
               
-              foco.valores(0.5, 0.5, 0);
+              //foco.valores(0.5, 0.5, 0);
               vertices[0] = new Punto(); vertices[0].valores(0f, 0f, 0f);
               vertices[1] = new Punto(); vertices[1].valores(1f, 0f, 0f);
               vertices[2] = new Punto(); vertices[2].valores(1f, 1f, 0f);
@@ -44,6 +46,11 @@ namespace ConsoleApplication1
             vertices[5] = new Punto(); vertices[5].valores(1f, 0f, 1f);
             vertices[6] = new Punto(); vertices[6].valores(1f, 1f, 1f);
             vertices[7] = new Punto(); vertices[7].valores(0f, 1f, 1F);
+            
+              vertices[0] = transformar.rotar(vertices[0], 45);
+              vertices[1] = transformar.rotar(vertices[1], 45);
+              vertices[2] = transformar.rotar(vertices[2], 45);
+              vertices[3] = transformar.rotar(vertices[3], 45);
 
         }
 
@@ -59,20 +66,21 @@ namespace ConsoleApplication1
             base.OnRenderFrame(e);
             GL.LoadIdentity();
             GL.Scale(0.5, 0.5, 0.5);
-            GL.Rotate(angulo, 0, 1, 0);
+          //  GL.Rotate(angulo, 0, 0, 1);
              
 
              GL.Begin(PrimitiveType.LineLoop);
-
+            
             GL.Vertex3(vertices[0].x, vertices[0].y, vertices[0].z);
+
             GL.Vertex3(vertices[1].x, vertices[1].y, vertices[1].z);
             GL.Vertex3(vertices[2].x, vertices[2].y, vertices[2].z);
             GL.Vertex3(vertices[3].x, vertices[3].y, vertices[3].z);
-            GL.Vertex3(vertices[4].x, vertices[4].y, vertices[4].z);
-            GL.Vertex3(vertices[5].x, vertices[5].y, vertices[5].z);
-            GL.Vertex3(vertices[6].x, vertices[6].y, vertices[6].z);
-            GL.Vertex3(vertices[7].x, vertices[7].y, vertices[7].z);
+          
             GL.End();
+
+        
+          
             if(angulo> 360)
             {
                 angulo = 0;
